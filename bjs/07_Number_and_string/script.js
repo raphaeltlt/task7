a79f7b6828f6eda8e41dcdc4f7caac4389d40447
raphaeltlt <raph2000@mail.rum>
2023-05-30 09:26:26 +0400

    update task

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -10,3 +10,46 @@ document.getElementById('btn_clr').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
+document.querySelector('.btn_num').addEventListener('click',  (e) => {
+    
+    const buttonInfo = e.target.innerHTML;
+
+    const setOperation = new Set(["+", "-", "*", "/"]);
+
+    if (setOperation.has(buttonInfo)){
+        lastOperand = parseInt(inputWindow.value);
+        operation = buttonInfo;
+        inputWindow.value = '';
+        }
+    else if (buttonInfo.charCodeAt(0) == 8730) {
+        inputWindow.value = Math.sqrt(parseInt(inputWindow.value));
+        operation = null;
+        }
+    else if (buttonInfo === '='){
+        const currentOperand = parseInt(inputWindow.value);    
+
+        if (operation === "+") {
+            inputWindow.value = lastOperand + currentOperand;
+            }
+        if (operation === "-") {
+            inputWindow.value = lastOperand - currentOperand;
+            }
+        if (operation === "*") {
+            inputWindow.value = lastOperand * currentOperand;
+            }
+        if (operation === "/") {
+            if (currentOperand === 0){
+                inputWindow.value = "error";
+                lastOperand = 0;
+                }
+            else{
+                inputWindow.value = lastOperand / currentOperand;
+                }    
+            }
+        operation = null;
+        }    
+    else{
+        inputWindow.value += buttonInfo;
+        }
+})
+
